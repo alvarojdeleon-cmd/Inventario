2af1606059d7a249cb5f3d01d6d73af74e42a9dc
alvarojdeleon <alvarojdeleon@gmail.com>
2026-09-13 23:00:56 -0500

    v1.1 se agrego la proteccion de credenciales

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
-// Crear conexión con MySQL
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "DeleonSierra",
-    database: "Inventario_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-// Conectar
 db.connect((error) => {
 
     if (error) {
